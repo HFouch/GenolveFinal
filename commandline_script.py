412b94663f46c424f19e27d96c6046a806b8d43b
HFouch <40229797+HFouch@users.noreply.github.com>
2020-09-17 09:05:39 +0200

    Update commandline_script.py

--- commandline_script.py
+++ commandline_script.py
@@ -163,9 +163,9 @@ def run(args):
     print('Number of most parsimonious solutions: ', len(shortest_paths))
     print()
     print('Average number of each operation per solution:')
-    print('Inversions: ', int(tot_inv/len(shortest_paths)), '  Transpositions type 1: ', int(tot_trp1/len(shortest_paths)), '  Transpositions type 2: ', int(tot_trp2/len(shortest_paths)), '  Balanced translocations: ', int(tot_b_trl/len(shortest_paths)), '  Unbalanced translocations: ', int(tot_u_trl/len(shortest_paths)),
-          '  Fusions: ', int(tot_fus/len(shortest_paths)),
-          '  Fissions: ', int(tot_fis/len(shortest_paths)))
+    print('Inversions: ', float(tot_inv/len(shortest_paths)), '  Transpositions type 1: ', float(tot_trp1/len(shortest_paths)), '  Transpositions type 2: ', float(tot_trp2/len(shortest_paths)), '  Balanced translocations: ', float(tot_b_trl/len(shortest_paths)), '  Unbalanced translocations: ', float(tot_u_trl/len(shortest_paths)),
+          '  Fusions: ', float(tot_fus/len(shortest_paths)),
+          '  Fissions: ', float(tot_fis/len(shortest_paths)))
     print()
     print()
     print('Solutions: ')
